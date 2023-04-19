0ac538c6c76ec0bb61d3fcabd38171e4ece7676a
efmomoh <efmomoh25@gmail.com>
2023-04-19 04:44:45 +0000

    Update script file

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const hamburger = document.querySelector(".hamburger");
-const menuList = document.querySelector(".menu-list");
+const hamburger = document.querySelector('.hamburger');
+const menuList = document.querySelector('.menu-list');
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    menuList.classList.toggle("active");
-})
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  menuList.classList.toggle('active');
+});
 
-document.querySelectorAll(".menu").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    menuList.classList.remove("active");
-}))
+document.querySelectorAll('.menu').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  menuList.classList.remove('active');
+}));
